4375afe7ecd855df4c6537902f0b34fecd56966e
ArsRumka <senarummo@gmail.com>
2025-10-07 23:32:29 +0300

    Добавьте файлы проекта.

---
+++ Task2/Data.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Threading;
+
+namespace Task2
+{
+    public static class Data
+    {
+        public static string ProcessData(string dataName)
+        {
+            Thread.Sleep(3000);
+            return $"Обработка {dataName} завершена за 3 секунды";
+        }
+
+        public static async Task<string> ProcessDataAsync(string dataName)
+        {
+            await Task.Delay(3000);
+            return $"Обработка {dataName} завершена за 3 секунды";
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(Data.ProcessData("Файл 1"));
+            Console.WriteLine(Data.ProcessData("Файл 2"));
+            Console.WriteLine(Data.ProcessData("Файл 3"));
+
+            Console.WriteLine("синхронная обработка данных завершена.");
+
+            Task<string> task1 = Data.ProcessDataAsync("Файл 1");
+            Task<string> task2 = Data.ProcessDataAsync("Файл 2");
+            Task<string> task3 = Data.ProcessDataAsync("Файл 3");
+
+            string[] tasksResult = await Task.WhenAll(task1, task2, task3);
+            
+            foreach (string result in tasksResult)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("асинхронная обработка данных завершена");
+        }
+    }
+}
